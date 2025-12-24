b16774940ad9640cc6779d735200d506d335d679
Nishkarsh Sahu <nishkarshsahu007@gmail.com>
2025-12-24 13:23:23 +0530

    removed extra space

--- app/controllers/api/v1/enrollments_controller.rb
+++ app/controllers/api/v1/enrollments_controller.rb
@@ -1,16 +1,13 @@
 class Api::V1::EnrollmentsController < ApplicationController
-
   # app/controllers/api/v1/enrollments_controller.rb
   def index
     enrollments = policy_scope(Enrollment)
-
     if params[:status].present?
       enrollments = enrollments.where(status: params[:status])
     end
-
     render json: enrollments
   end
-  
+
   def create
     enrollment = Enrollment.new(enrollment_params.merge(student: current_user))
     authorize enrollment
